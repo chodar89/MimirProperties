d616ec378a479890c13abe7bb8190ee8de2066db
Tobiasz <tobiasz0889@gmail.com>
2023-01-22 14:16:21 +0000

    :sparkles: Add AddressModel

---
+++ src/mimir/web_app/infrastructure/repositories/models/address.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from .base import EntityBaseModel
+
+
+class AddressModel(EntityBaseModel, models.Model):
+
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    line1 = models.CharField(max_length=255)
+    line2 = models.CharField(max_length=255, null=True)
+    line3 = models.CharField(max_length=255, null=True)
