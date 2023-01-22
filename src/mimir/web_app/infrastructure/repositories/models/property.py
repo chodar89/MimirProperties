cd1931e354ae8322ae2503f9045261a7521a890b
Tobiasz <tobiasz0889@gmail.com>
2023-01-22 14:14:29 +0000

    :art: Sort PropertyModel attributes

--- src/mimir/web_app/infrastructure/repositories/models/property.py
+++ src/mimir/web_app/infrastructure/repositories/models/property.py
@@ -5,13 +5,13 @@ from .base import EntityBaseModel
 
 class PropertyModel(EntityBaseModel, models.Model):
 
-    title = models.CharField(max_length=255)
     area_square_meters = models.IntegerField()
-    rooms = models.IntegerField()
+    built_year = models.PositiveIntegerField()
     floor = models.IntegerField(null=True)
     is_new_build = models.BooleanField(default=True)
-    built_year = models.PositiveIntegerField()
     listing_date = models.DateField()
-    listing_updated_date = models.DateField(null=True)
     listing_reference_number = models.CharField(max_length=255)
+    listing_updated_date = models.DateField(null=True)
     phone_number = models.CharField(max_length=255)
+    rooms = models.IntegerField()
+    title = models.CharField(max_length=255)
