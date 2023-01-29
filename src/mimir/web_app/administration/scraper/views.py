8cde41f615428cedd70576365decc5db761ecca7
Tobiasz <tobiasz0889@gmail.com>
2023-01-29 17:45:15 +0000

    :sparkles: Add skeleton for ScrapeDataView

--- src/mimir/web_app/administration/scraper/views.py
+++ src/mimir/web_app/administration/scraper/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, View
 
 from shared.value_objects.listing_service.listings import ListingServiceName
 
@@ -12,3 +14,12 @@ class ListingServicesView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context["listing_services"] = [i for i in ListingServiceName]
         return context
+
+
+class ScrapeDataView(View):
+
+    template_name = "admin/scrape_data.html"
+
+    def get(self, request: HttpRequest, *args: tuple, **kwargs: dict) -> HttpResponse:
+        # TODO: call ScrapeService and process. Will need to do as background task
+        return redirect("listing_services")
