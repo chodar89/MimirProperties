c0d67a05710f8814e093bb270884fed4a2c6cb36
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-02-05 16:47:13 +0000

    ✨ Add PropertyType enum

--- src/shared/value_objects/listing_service/listings.py
+++ src/shared/value_objects/listing_service/listings.py
@@ -3,3 +3,8 @@ from enum import StrEnum  # type: ignore
 
 class ListingServiceName(StrEnum):
     OTODOM = "OTODOM"
+
+
+class PropertyType(StrEnum):
+    APARTAMENT = "APARTAMENT"
+    HOUSE = "HOUSE"
