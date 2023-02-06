484b9e9940132598ee13febd15a0100a1202c2d1
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-02-06 18:19:07 +0000

    ✨ Add min and max price to ScraperForm

--- src/mimir/web_app/administration/forms.py
+++ src/mimir/web_app/administration/forms.py
@@ -3,11 +3,11 @@ from django import forms
 from shared.value_objects.listing_service.listings import PropertyType
 
 AREA = [
-    (5, "< 5 miles"),
-    (10, "10 miles"),
-    (20, "20 miles"),
-    (40, "40 miles"),
-    (50, "> 50 miles"),
+    (5, "< 5 km"),
+    (10, "10 km"),
+    (20, "20 km"),
+    (40, "40 km"),
+    (50, "> 50 km"),
 ]
 
 
@@ -15,3 +15,5 @@ class ScraperForm(forms.Form):
     property_type = forms.TypedChoiceField(choices=PropertyType)
     city = forms.CharField(max_length=100)
     area = forms.CharField(required=True, label="Range area:", widget=forms.Select(choices=AREA))
+    min_price = forms.CharField(max_length=20)
+    max_price = forms.CharField(max_length=20)
