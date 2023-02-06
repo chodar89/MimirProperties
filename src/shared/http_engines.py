7f51755a7c70e73e553813d479d9b5f5c3366dfa
Tobiasz <tobiasz0889@gmail.com>
2023-02-06 18:13:54 +0000

    Add HttpEngine abstract class and RequestsEngine concret class

---
+++ src/shared/http_engines.py
@@ -0,0 +1,46 @@
+import logging
+from abc import ABC as abstract
+from abc import abstractmethod
+from concurrent.futures import ThreadPoolExecutor
+
+import requests
+
+from shared.dto import HttpRequestDto, HttpResponseDto
+
+logger = logging.getLogger(__name__)
+
+
+class HttpEngine(abstract):
+    @abstractmethod
+    def process_request(self, dto: HttpRequestDto) -> HttpResponseDto:
+        ...
+
+    @abstractmethod
+    def process_async_requests(self, dto_list: list[HttpRequestDto]) -> list[HttpResponseDto]:
+        ...
+
+
+class RequestsEngine(HttpEngine):
+    def process_request(self, dto: HttpRequestDto) -> HttpResponseDto:
+        try:
+            resp = self._request(dto)
+            return HttpResponseDto(request_dto=dto, json=resp.json(), body_text=resp.text)
+        except Exception as e:
+            logger.info("Exception raised during request. Error: %s", e)
+        return HttpResponseDto(request_dto=dto)
+
+    def process_async_requests(self, dto_list: list[HttpRequestDto]) -> list[HttpResponseDto]:
+        with ThreadPoolExecutor() as executor:
+            results = executor.map(self.process_request, dto_list)
+        return list(results)
+
+    def _request(self, dto: HttpRequestDto) -> requests.Response:
+        req_method = getattr(requests, dto.http_method.lower())
+        resp: requests.Response = req_method(
+            dto.URL,
+            params=dto.query_parameters,
+            headers=dto.headers,
+            data=dto.payload,
+            timeout=dto.timeout,
+        )
+        return resp
