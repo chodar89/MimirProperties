ee0660b2545f4c961f111a41518563323f0737d2
Tobiasz <tobiasz0889@gmail.com>
2023-02-18 18:46:04 +0000

    :recycle: Add django validation for URL

--- src/shared/types/http.py
+++ src/shared/types/http.py
@@ -1,9 +1,19 @@
+import logging
 from typing import Any
+
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+
+log = logging.getLogger(__name__)
 
 
 class URL(str):
     def __new__(cls, string: Any) -> "URL":
         url = super().__new__(cls, string)
-        if not url.startswith("http://") or not url.startswith("https://"):
+        url_validator = URLValidator(verify_exists=False)
+        try:
+            url_validator(url)
+        except ValidationError as e:
+            log.info("Invalid URL. Error: %s", e)
             raise ValueError("Invalid URL")
         return url
